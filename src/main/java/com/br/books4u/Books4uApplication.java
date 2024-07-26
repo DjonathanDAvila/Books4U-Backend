e5bcb7fe132dbc03f914b2bcfb322958e3e3c3ea
Djonathan D'Avila <123762143+DjonathanDAvila@users.noreply.github.com>
2024-07-26 20:52:16 -0300

    :tada: First commit

---
+++ src/main/java/com/br/books4u/Books4uApplication.java
@@ -0,0 +1,13 @@
+package com.br.books4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Books4uApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Books4uApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/books4u/Books4uApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.books4u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Books4uApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
