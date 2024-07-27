e6a2c6c0e8d1f6cae8ddfb11e33b04ddf80eaf93
Djonathan D'Avila <123762143+DjonathanDAvila@users.noreply.github.com>
2024-07-27 20:43:46 -0300

    :sparkles: feat: Finished complete crud Publisher

--- src/main/java/com/br/books4u/domain/publisher/PublisherRepository.java
+++ src/main/java/com/br/books4u/domain/publisher/PublisherRepository.java
@@ -3,6 +3,9 @@ package com.br.books4u.domain.publisher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+    List<Publisher> findByNameContaining(String nome);
 }
