b824ff0e29453e2be140bd4113e925d39053dcd3
Djonathan D'Avila <123762143+DjonathanDAvila@users.noreply.github.com>
2024-07-28 18:49:32 -0300

    :sparkles: feat: Finished complete crud BookLocalization

--- src/main/java/com/br/books4u/domain/bookLocalization/BookLocalizationService.java
+++ src/main/java/com/br/books4u/domain/bookLocalization/BookLocalizationService.java
@@ -2,9 +2,31 @@ package com.br.books4u.domain.bookLocalization;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookLocalizationService {
     @Autowired
     private BookLocalizationRepository bookLocalizationRepository;
+
+    @Transactional(readOnly = true)
+    public List<BookLocalization> findAll() {
+        return bookLocalizationRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<BookLocalization> findById(Long id) {
+        return bookLocalizationRepository.findById(id);
+    }
+
+    public BookLocalization save(BookLocalization bookLocalization) {
+        return bookLocalizationRepository.save(bookLocalization);
+    }
+
+    public void deleteById(Long id) {
+        bookLocalizationRepository.deleteById(id);
+    }
 }
